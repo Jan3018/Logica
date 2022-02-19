ddadd133e373ef79357da9f76d891b2e3a275185
Jan3018 <99089334+Jan3018@users.noreply.github.com>
2022-02-18 23:06:04 -0600

    Área y perímetro

---
+++ 2.areayperimetro.py
@@ -0,0 +1,10 @@
+Radio = float(input("Escribe el radio: "))
+
+pi = 3.14
+constante =  2
+
+perimetro = constante * pi * Radio
+area = pi * pow(Radio,2)
+
+print("perimetro: " + str(perimetro))
+print( "Área: " + str(area))
