9be920611fc224b1eab812da6f54e23d7a31644b
Jan3018 <99089334+Jan3018@users.noreply.github.com>
2022-02-19 13:32:25 -0600

    Funciones

---
+++ Funciones.py
@@ -0,0 +1,20 @@
+#FUNCIÓN
+#SINTAXIS
+# def Nombre_de_la_función 
+
+from http.client import MULTIPLE_CHOICES
+
+
+def Suma(a,b):
+    suma = a + b
+    return suma
+
+def Multiplicación(a, b):
+    return a * b
+
+def División (a,b):
+    return a / b
+
+print(f"La suma = {Suma(10,5)}")
+print(f"La Multiplicación = {Multiplicación(2,30)}")
+print(f"La División = {División(12,4)}")
